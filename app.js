9e5fe9f1341ef6877e6bdf1fa1a7dfa3b66c3aa7
Oliver Sun <yuhuisun940531@gmail.com>
2019-12-29 14:35:08 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const Battle = require('./lib/battle.js');
+const fs = require("fs");
+const Battle = require("./lib/battle.js");
 
-let setting = JSON.parse(fs.readFileSync('setting.json'));
-let config = JSON.parse(fs.readFileSync('config.json'));
+let setting = JSON.parse(fs.readFileSync("setting.json"));
+let config = JSON.parse(fs.readFileSync("config.json"));
 let battle = new Battle(setting, config);
 
 battle.start();
